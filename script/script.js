8417aed45a533a8c374419ccfe5c5db22923c5c8
yingruicheah <ruiruicheah@hotmail.com>
2024-03-23 12:47:53 +0800

    Include Mobile
    include mobile

--- script/script.js
+++ script/script.js
@@ -1,11 +1,22 @@
 function clickrule() {
 			document.getElementById('openButton').addEventListener('click', function() {
-			document.getElementById('popupcontainer').style.display = 'block';
+			
+			if(window.innerWidth >=768){
+				document.getElementById('popupcontainer').style.display = 'block'
+			}
+			else{
+				document.getElementById('popupcontainermobile').style.display = 'block'
+			}
 			});
 
 			document.getElementById('closeButton').addEventListener('click', function() {
 			  document.getElementById('popupcontainer').style.display = 'none';
 			});
+			
+			document.getElementById('closeButtonMobile').addEventListener('click', function() {
+			  document.getElementById('popupcontainermobile').style.display = 'none';
+			});
+			
 		}
 function refresh(){
         window.location.reload("Refresh")
